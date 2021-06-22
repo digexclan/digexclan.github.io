6c56c1ea7fd6938e8feaa8649790e0ea999a19f3
Somesh Kar <gh@somesh.co>
2021-06-23 01:05:01 +0530

    Updates

---
+++ 1bfc9850-acce821eab0f634d575f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{ma3e:function(t,c,n){"use strict";n.d(c,"a",(function(){return e}));var a=n("Lnxd");function e(t){return Object(a.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zm113.9 231L234.4 103.5c-9.4-9.4-24.6-9.4-33.9 0l-17 17c-9.4 9.4-9.4 24.6 0 33.9L285.1 256 183.5 357.6c-9.4 9.4-9.4 24.6 0 33.9l17 17c9.4 9.4 24.6 9.4 33.9 0L369.9 273c9.4-9.4 9.4-24.6 0-34z"}}]})(t)}}}]);
+//# sourceMappingURL=1bfc9850-acce821eab0f634d575f.js.map
---
+++ component---src-pages-404-js-bec90c792eca9c3ff084.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"404 :("),l.a.createElement("p",null,"What you're looking for doesn't seem to exist."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bec90c792eca9c3ff084.js.map
